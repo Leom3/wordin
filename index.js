41be8c9e0cd21d7d51b092cbd837183148b60757
lmaignan <leo.maignan@epitech.eu>
2020-05-06 21:56:49 +0200

    🍻 login and create game db code

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const port = config.port;
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const game = require('./src/game')
 const bearerToken = require('express-bearer-token');
 const socket = require('socket.io');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const database = require('./src/database')
 
 var server = app.listen(8000);
 var io = require('socket.io').listen(server);
@@ -22,10 +22,73 @@ io.use(function (socket, next) {
 	sessionMiddleware(socket.request, socket.request.res, next);
 });
 
+//Usefull mongo functions
+//database.findOneInCollection("Wallets", {"userId" : user_id, "walletType" : "coinbase"}, function(err, wallet) {}
+//database.insertInCollection("Wallets", walletData, function(err, wallet) {
+//database.updateInCollection("Wallets", {"userId" : userId, "walletType" : "coinbase"}, {"$set": {accessToken: tokens.access_token, refreshToken: tokens.refresh_token}}, function(err, updated) {
+//	if (err) {
+//		return 84;
+//	}
+//	if (updated.modifiedCount != 1) {
+//		return 84;
+//	}
+
 io.on('connection', function (socket) {
 	socket.on('login', function(data) {
 		console.log(data);
-		io.sockets.sockets[socket.id].emit('logged', "pute " + data + " !");
+		var username = data.username;
+		socketId = socket.id
+		database.findOneInCollection("users", {"username" : username}, function(err, user) {
+			if (err) {
+				console.log(err);
+				io.sockets.sockets[socket.id].emit('error', err);
+			}
+			else {
+				if (user) {
+					console.log("already logged");
+					io.sockets.sockets[socket.id].emit('used', "Username " + username  + " already used !");
+				}	
+				else {
+					database.findInCollection("game", {}, function(err, items) {
+						if (err) {
+							io.sockets.sockets[socket.id].emit('error', err);
+						}
+						console.log(items);
+						if (items.length <= 0) {
+							database.insertInCollection("game", {"host" : username, "socketId" : socketId, "type" : "gameRoom", players : [username]}, function(err, game) {
+								if (err)
+									io.sockets.sockets[socket.id].emit('error', err);
+								else {
+									database.insertInCollection("users", {"username" : username, "socketId" : socketId}, function(err, game) {
+										if (err)
+											io.sockets.sockets[socket.id].emit('error', err);	
+										else
+											io.sockets.sockets[socket.id].emit('logginHost', username);
+									});
+								}
+							});
+						}
+						else {
+							database.insertInCollection("users", {"username" : username, "socketId" : socketId}, function(err, game) {
+								if (err)
+									io.sockets.sockets[socket.id].emit('error', err);
+							});
+	
+							database.updateInCollection("game", {"type" : "gameRoom"}, {"$push": {"players": username}}, function(err, updated) {
+								if (err)
+									io.sockets.sockets[socket.id].emit('error', err);
+								if (updated.modifiedCount != 1) {
+									console.log("ERROR UPDATING2");
+								}
+								else {
+									io.sockets.sockets[socket.id].emit('loggin', username);
+								}
+							});
+						}
+					});	
+				}
+			}
+		});
 	});
 });
 
@@ -41,9 +104,7 @@ app.set('views', __dirname + 'public/views');
 
 app.use(bearerToken());
 
-app.use('/game', game);
-
 app.get('/', function(req, res) {
 	res.status(200);
-	res.sendFile(__dirname + "/public/views/index.html")
+	res.sendFile(__dirname + "/public/views/test.html")
 });
