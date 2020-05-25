0737ba56aacc8c89157730647ac44b2ae1a2d21d
Matthias Gayaud <matthias.gayaud@epitech.eu>
2020-05-26 02:10:22 +0700

    :sparkles: reset game button + player votes (only incremental)
    reset game button + player votes (only incremental)

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 const socket = io();
 var userName = "";
 var turn = 0;
+let isHost = false;
 
 $('.usernameForm').submit((e) => {
   e.preventDefault(); // prevents page reloading
@@ -15,13 +16,25 @@ $('.usernameForm').submit((e) => {
 socket.on('players', (players) => {
   $('#usernameList').html("");
   $(".playerList").html("");
+  $(".votePlayerList").html("");
+  let i = 0;
   for (player of players) {
     $('#usernameList').append($('<span class="username">').text(player));
     $(".playerList").append(`<div class="player">${player}</div>`);
+    $(".votePlayerList").append(`
+      <div class="votePlayer">
+        <input id="${i}player" type="radio" name="player" onchange="newVote(this)" />
+         ${player}
+        <span id="${i}playerVotes"></span>
+      </div>`);
+    i++;
   }
 });
 
 socket.on('loggedHost', (msg) => {
+  isHost = true;
+  $(".goToVotesButton").removeClass("hide");
+  $(".validateVotesButton").removeClass("hide");
   $('.lobbyContainer').append($('<button class="startGameButton">').text("Start game"));
   $(".startGameButton").on("click", function() {
     socket.emit('startGame', "");
@@ -52,4 +65,40 @@ $('.sendWordForm').submit((e) => {
 
 socket.on("getClue", (data) => {
   $(`.player`)[data.index].append(` ${data.msg} `);
+});
+
+$(".goToVotesButton").click(() => {
+  socket.emit("switchToVote", "");
+});
+
+socket.on("switchToVote", () => {
+  $(".gameContainer").addClass("hide");
+  $(".voteContainer").removeClass("hide");
+});
+
+function newVote(element) {
+  socket.emit("addVote", {"id": parseInt($(element).attr("id"))});
+}
+
+socket.on("voteCount", (data) => {
+  $(`#${data.id}playerVotes`).text(` ${data.nbVotes} `);
+});
+
+$(".resetButton").click(() => {
+  socket.emit("reset", "");
+});
+
+socket.on("onReset", () => {
+  isHost = false;
+  $('#usernameList').html("");
+  $(".playerList").html("");
+  $(".votePlayerList").html("");
+  $(".goToVotesButton").addClass("hide");
+  $(".gameContainer").addClass("hide");
+  $(".voteContainer").addClass("hide");
+  $(".validateVotesButton").addClass("hide");
+  $(".lobbyContainer").removeClass("hide");
+  $('.addUsernameButton').removeClass("removed");
+  $(".startGameButton").addClass("hide");
+  $('#usernameInput').attr("readonly", false);
 });
